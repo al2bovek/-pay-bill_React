dfebbb91b5292a77f9f83ddb079b04b5a03fde4f
Aleksandr Dubovik <95865961+al2bovek@users.noreply.github.com>
2026-01-22 15:46:26 +0200

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/-/',
+  base: '/-pay-bill_React/',
   plugins: [react(),
   tailwindcss(),
   ],
